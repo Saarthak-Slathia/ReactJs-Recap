9968489fb937ffc05d0ad23c8e6282b0b7375806
Saarthak Slathia <77338782+Saarthak-Slathia@users.noreply.github.com>
2022-05-30 02:34:02 +0000

    Add other components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
+import {Todos} from "./components/Todos";
+import TodoItem from "./components/TodoItem";
+import {Footer} from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header />
+      <Todos />
+      <Footer />
     </>
   );
 }
